6f9ab90722e6b565cf18dc9db09ae8ef32300172
AliceChurchfield <mariongarmel@gmail.com>
2023-06-10 13:34:08 -0700

    Just getting some initial testable stuff in
    The main acbc.js loader should be ready. Currently it just sets up the version number, loads the BC ModSDK and registers ACBC with it, and then loads the acbc-util.js file.

---
+++ acbc-util.js
@@ -0,0 +1,16 @@
+/**
+ * acbc-util.js
+ * 
+ * @file
+ *   Utility functions and properties
+ * @author   Alice Churchfield
+ */
+
+
+
+if (ACBC_VERSION === undefined)
+  console.warn("Running acbc-util.js outside of acbc.js");
+
+
+
+console.log(" * acbc-util.js loaded.");
