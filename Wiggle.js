e028a24f269ecc23c5538f4419ae49cdcb9de5bb
AliceChurchfield <mariongarmel@gmail.com>
2023-06-11 21:42:28 -0700

    Still working on the DrawCharacter hook

--- Wiggle.js
+++ Wiggle.js
@@ -18,40 +18,31 @@ if (!window.ACBC)
 ACBC.Wiggling = false;
 
 /**
- * The type of the DrawCharacter function
- * @callback DrawCharacterFunction
- * @param {Character} C - Character to draw
- * @param {number} X - Position of the character on the X axis
- * @param {number} Y - Position of the character on the Y axis
- * @param {number} Zoom - Zoom factor
- * @param {boolean} [IsHeightResizeAllowed=true] - Whether or not the settings allow for the height modifier to be applied
- * @param {CanvasRenderingContext2D} [DrawCanvas] - The canvas to draw to; If undefined `MainCanvas` is used
- * @returns {void} - Nothing
- */
-
-/**
  * Draws the character with their position modified as necessary by wiggling
- * @param {DrawCharacterFunction} next - The next function in the hook chain
- * @param {Character} C - Character to draw
- * @param {number} X - Position of the character on the X axis
- * @param {number} Y - Position of the character on the Y axis
- * @param {number} Zoom - Zoom factor
- * @param {boolean} [IsHeightResizeAllowed=true] - Whether or not the settings allow for the height modifier to be applied
- * @param {CanvasRenderingContext2D} [DrawCanvas] - The canvas to draw to; If undefined `MainCanvas` is used
+ * @param {Array} args - The arguments to pass to next
+ * @param {(args: Array) => void} next - The next function in the hook chain
  * @returns {void} - Nothing
  */
-ACBC.WiggleCharacter = function(next, C, X, Y, Zoom, IsHeightResizeAllowed, DrawCanvas)
+ACBC.WiggleCharacter = function(args, next)
 {
-  if (!C?.ACBC?.Wiggling)
-    return next(C, X, Y, Zoom, IsHeightResizeAllowed, DrawCanvas);
-  
-  X += 100;
-  return next(C, X, Y, Zoom, IsHeightResizeAllowed, DrawCanvas);
+  /** @type {Character} */
+  let C = args[0];
+  /** @type {number} */
+  let X = args[1];
+  /** @type {number} */
+  let Y = args[2];
+  /** @type {number} */
+  let Zoom = args[3];
+  /** @type {boolean} */
+  let IsHeightResizeAllowed = args[4];
+  /** @type {CanvasRenderingContext2D} */
+  let DrawCanvas = args[5];
+  return next(args);
 }
 
 ACBC.ModApi.hookFunction("DrawCharacter", 0, (args, next) =>
 {
-  return ACBC.WiggleCharacter(next, ...args);
+  return ACBC.WiggleCharacter(args, next);
 });
 
 
